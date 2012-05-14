c652e713e056690e932161d079c1f484de52a9a9
Jehan Bruggeman <jehan.bruggeman@euranova.eu>
2012-05-14 14:03:17 +0200

    Small script to merge from local-dev to develop.

---
+++ local-dev_to_develop.sh
@@ -0,0 +1,12 @@
+git checkout develop
+read -p "ok?"
+git pull origin develop
+read -p "ok?"
+git checkout local-dev
+read -p "ok?"
+git rebase develop
+read -p "ok?"
+git checkout develop
+echo -e "Please merge with a description of what you are merging: \
+\n\tgit merge local-dev --no-ff -m \"[DESCRIPTION]\" \
+\n\tgit push origin develop"
