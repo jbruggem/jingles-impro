f019dce0d244e58ba77a1b2f18ae4e53196d16d2
alex <alex.derman.filter@gmail.com>
2012-12-11 18:51:52 +0100

    implemented an animated transition between widgets

--- src/qt-ui/testui.cpp
+++ src/qt-ui/testui.cpp
@@ -1,24 +1,89 @@
 
 #include "testui.h"
 
-#include <QGridLayout>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QPropertyAnimation>
+#include <QParallelAnimationGroup>
 #include "stocklistwidget.h"
 #include "playlistwidget.h"
 #include "tracklist.h"
 
 TestUi::TestUi(QWidget *parent)
     : QWidget (parent) {
-
-    stockListWidget = new StockListWidget;
-    playListWidget  = new PlayListWidget;
+        
+    w1 = new StockListWidget;
+    w2 = new PlayListWidget;
+    w3 = new PlayListWidget;
     
-    setLayout(new QGridLayout);
-    // layout()->addWidget(stockListWidget);
-    layout()->addWidget(playListWidget);
+    button = new QPushButton(tr("switch"));
+    
+    QHBoxLayout *l = new QHBoxLayout;
+    setLayout(l);
+    l->addWidget(button);
+    l->addWidget(w1);
+    l->addWidget(w2);
+    l->addWidget(w3);
+    
+    state = W3;
+    w1->setMaximumWidth(0);
+    
+    // setup animations
+    w1Width = new QPropertyAnimation(w1, "maximumWidth");
+    w1Width->setDuration(500);
+    w1Width->setStartValue(0);
+    // end value to be defined when starting the transition
+    w1Width->setEasingCurve(QEasingCurve::InOutQuad);
+    
+    w3Width = new QPropertyAnimation(w3, "maximumWidth");
+    w3Width->setDuration(500);
+    // start value to be defined when starting the transition
+    w3Width->setEndValue(0);
+    w3Width->setEasingCurve(QEasingCurve::InOutQuad);
+    
+    transition = new QParallelAnimationGroup;
+    transition->addAnimation(w1Width);
+    transition->addAnimation(w3Width);
+    
+    connect(button,     SIGNAL(clicked()),  this, SLOT(buttonPressed()));
+    connect(transition, SIGNAL(finished()), this, SLOT(transitionFinished()));
 }
 
 void TestUi::setTrackList(TrackList *list) {
-    stockListWidget->setTrackList(list);
-    playListWidget->setTrackList(list);
+    w1->setTrackList(list);
+    w2->setTrackList(list);
+    w3->setTrackList(list);
+}
+
+void TestUi::buttonPressed() {
+    QLOG_TRACE() << "TestUi::buttonPressed()";
+    
+    if (state == W3) {
+        w1Width->setEndValue(w3->width());
+        w3Width->setStartValue(w3->width());
+        button->setEnabled(false);
+        state = W1;
+        transition->setDirection(QAbstractAnimation::Forward);
+        transition->start();
+        
+    } else if (state == W1) {
+        w1Width->setEndValue(w1->width());
+        w3Width->setStartValue(w1->width());
+        button->setEnabled(false);
+        state = W3;
+        transition->setDirection(QAbstractAnimation::Backward);
+        transition->start();
+    }
+}
+
+void TestUi::transitionFinished() {
+    QLOG_TRACE() << "TestUi::transitionFinished()";
+    
+    if (state == W1) {
+        w1->setMaximumWidth(QWIDGETSIZE_MAX);
+    } else if (state == W3) {
+        w3->setMaximumWidth(QWIDGETSIZE_MAX);
+    }
+    button->setEnabled(true);
 }
 
--- src/qt-ui/testui.h
+++ src/qt-ui/testui.h
@@ -4,17 +4,37 @@
 
 #include <QWidget>
 
+class QPushButton;
 class StockListWidget;
 class PlayListWidget;
 class TrackList;
+class QPropertyAnimation;
+class QParallelAnimationGroup;
 
 class TestUi : public QWidget {
+    
+    Q_OBJECT
+    
     public:
         TestUi(QWidget *parent = 0);
         void setTrackList(TrackList *);
+    private slots:
+        void buttonPressed();
+        void transitionFinished();
     private:
-        StockListWidget *stockListWidget;
-        PlayListWidget  *playListWidget;
+        QPushButton     *button;
+        StockListWidget *w1;
+        PlayListWidget  *w2;
+        PlayListWidget  *w3;
+        
+        // animations
+        enum State {
+            W1,
+            W3
+        } state;
+        QPropertyAnimation *w1Width;
+        QPropertyAnimation *w3Width;
+        QParallelAnimationGroup *transition;
 };
 
 #endif
